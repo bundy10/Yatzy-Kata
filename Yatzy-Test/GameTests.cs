66990b13effef826d1732646afa8b7221e8cba9c
bundy huang <bundy.huang@myob.com>
2023-01-24 02:32:16 +1100

    refactor: refactored constructor to take the player and roundfactory mocks

--- Yatzy-Test/GameTests.cs
+++ Yatzy-Test/GameTests.cs
@@ -4,69 +4,64 @@ using Yatzy_Kata.Interfaces;
 
 namespace Yatzy_Test;
 
-//ok
 
 public class GameTests
 {
-    private readonly Mock<IPlayer> _player1Mock;
-    private readonly Mock<IPlayer> _player2Mock;
+    private readonly Mock<IRoundFactory> _roundFactoryMock;
+    private readonly List<Mock<IPlayer>> _playerMocks;
+    private readonly Game _game;
+
     public GameTests()
     {
-        _player1Mock = new Mock<IPlayer>();
-        _player2Mock = new Mock<IPlayer>();
+        _roundFactoryMock = new Mock<IRoundFactory>();
+        _playerMocks = new List<Mock<IPlayer>> { new(), new() };
+        _game = new Game(_playerMocks.Select(playerMock => playerMock.Object), _roundFactoryMock.Object);
     }
     [Fact]
     public void GivenPlayGameIsCalled_ThenGamePromptsPlayerToPlayAgain()
     {
         //Arrange
-        var game = new Game(new[] { _player1Mock.Object});
-        _player1Mock.Setup(player => player.PlayAgain()).Verifiable();
+        _playerMocks[0].Setup(player => player.PlayAgain()).Verifiable();
         
         //Act
-        game.PlayGame();
+        _game.PlayGame();
         
         //Assert
-        _player1Mock.Verify();
+        _playerMocks[0].Verify();
     }
 
     [Fact]
     public void PlayGame_DoesNotAskPlayerTwoToPlayAgain_WhenPlayerOneDoesNotPlayAgain()
     {
-        var game = new Game(new[] { _player1Mock.Object, _player2Mock.Object });
-        _player1Mock.Setup(player => player.PlayAgain()).Returns(false);
+        _playerMocks[0].Setup(player => player.PlayAgain()).Returns(false);
 
-        game.PlayGame();
+        _game.PlayGame();
 
-        _player2Mock.Verify(player => player.PlayAgain(), Times.Never);
+        _playerMocks[1].Verify(player => player.PlayAgain(), Times.Never);
     }
 
     [Fact]
     public void GivenPlayGameIsCalled_WhenTheGameIsOver_ThenThePlayerWithTheMostPointsWillBePrompted()
     {
-        var game = new Game(new[] { _player1Mock.Object, _player2Mock.Object });
-        _player1Mock.Setup(player => player.TotalPoints).Returns(100);
-        _player2Mock.Setup(player => player.TotalPoints).Returns(50);
+        _playerMocks[0].Setup(player => player.TotalPoints).Returns(100);
+        _playerMocks[1].Setup(player => player.TotalPoints).Returns(50);
         
-        _player1Mock.SetupSet(player => player.Winner = It.IsAny<bool>()).Verifiable();
+        _playerMocks[0].SetupSet(player => player.Winner = It.IsAny<bool>()).Verifiable();
         
-        game.PlayGame();
+        _game.PlayGame();
 
-        _player1Mock.VerifySet(player => player.Winner = true);
+        _playerMocks[0].VerifySet(player => player.Winner = true);
     }
 
     [Fact]
     public void GivenPlayGameIsCalled_WhenTheGameStarts_ThenARoundIsCreated()
     {
-        //Arrange
-        var roundFactoryMock = new Mock<IRoundFactory>();
-        var game = new Game(new[] { _player1Mock.Object, _player2Mock.Object }, roundFactoryMock.Object);
-        
         
         //Act
-        game.PlayGame();
+        _game.PlayGame();
 
         //Assert
-        roundFactoryMock.Verify(roundFactory => roundFactory.CreateRound(), Times.AtLeast(1));
+        _roundFactoryMock.Verify(roundFactory => roundFactory.CreateRound(), Times.AtLeast(1));
     }
 
 }
