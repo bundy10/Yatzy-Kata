61a8b9e29488b993ca8bcbd7da37df09656c475e
bundy huang <bundy.huang@myob.com>
2023-02-17 15:41:11 +1100

    feat: added some more data to roundoutcome records

--- Yatzy-Kata/Records/RoundOutcomes.cs
+++ Yatzy-Kata/Records/RoundOutcomes.cs
@@ -4,9 +4,6 @@ namespace Yatzy_Kata.Outcomes;
 
 public record RoundOutcomes();
 
-public sealed record RoundOver : RoundOutcomes;
-
-public sealed record RoundWinner : RoundOutcomes
-{
-    public IPlayer Winner;
-}
+public sealed record RoundOver() : RoundOutcomes;
+public sealed record RoundTie(int Score, IEnumerable<IPlayer> Players) : RoundOutcomes;
+public sealed record RoundWinner(int Score, IPlayer Player) : RoundOutcomes;
