54aec5d157c4fea75bc5eff5cf2116bb06d08ad1
bundy huang <bundy.huang@myob.com>
2023-02-27 04:39:11 +1100

    feat: adding constant strings

---
+++ Yatzy-Kata/ConsoleMessages.cs
@@ -0,0 +1,9 @@
+namespace Yatzy_Kata;
+
+public class ConsoleMessages
+{
+    public const string Welcome = "Welcome to Yatzy";
+    public const string Farewell = "bye";
+    public const string CategoryChoice = "please select a category";
+    
+}
